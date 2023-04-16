6be7d8f8635227cf89d5846c7ecf128b49186205
Shivam Yadav <yadavshivam24012002@gmail.com>
2023-04-16 12:54:42 +0530

    mycart system tested and integrated to db

--- functionality/cart.js
+++ functionality/cart.js
@@ -1,93 +1,114 @@
 import { getElement } from "../utils/util.js";
-const container = getElement('.container');
+const container = getElement(".container");
 
 var httpRequest;
-const url = 'http://localhost:9090/api/cart/1';
-var food_url = 'http://localhost:9090/api/item/'
-var food_ids = [];
+const url = "http://localhost:9090/api/cart/1";
+var food_url = "http://localhost:9090/api/item/";
+var imgURL = "http://localhost:9090/api/item/image/";
+var food_ids ;
 var foodItems = [];
+var cart;
 
 makeRequest(url);
 
-function makeRequest(url){
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET',url,true);
+function makeRequest(url) {
+  httpRequest = new XMLHttpRequest();
+  httpRequest.open("GET", url, true);
 
-    httpRequest.onreadystatechange = getCartItems;
-    httpRequest.setRequestHeader('Authorization','Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaGl2YW1AZ21haWwuY29tIiwiZXhwIjoxNjgxNDg5MzY3LCJpYXQiOjE2ODE0ODc1Njd9.Cc6L3cUFtVrssO9HxiARdCBkzOuY9yJM3uEX_Y0wtBWoFTSg3UYdbAg9DqOeZGtRvYUEnc_lZa49aggJ19Wm6A');
-    httpRequest.send();
+  httpRequest.onreadystatechange = getCartItems;
+  httpRequest.setRequestHeader(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaGl2YW1AZ21haWwuY29tIiwiZXhwIjoxNjgxNjMxMDY0LCJpYXQiOjE2ODE2MjkyNjR9.M7iVZ8npycgcnGg0-G7DJKAwxD8A9IC1ThbrdVrOAAoXaBBRPeWuis438e_7HEI-J9gFfL7fHc-LJ77UyRbMvQ"
+  );
+  httpRequest.send();
 }
 
+function getCartItems() {
+  if (
+    httpRequest.readyState === XMLHttpRequest.DONE &&
+    httpRequest.status === 200
+  ) {
+    const response = JSON.parse(httpRequest.responseText);
+    console.log("cart: " + JSON.stringify(response));
+    cart = response;
+    console.log('food item: '+ JSON.stringify(response.food_item));
+    food_ids = response.food_item;
+    const countCards = Object.keys(food_ids).length;
+    const firstKey = Object.keys(food_ids)[0];
+    // console.log(response.food_item);
 
-function getCartItems(){
-    if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-        const response = JSON.parse(httpRequest.responseText);
-        // console.log(response.food_item);
-        // console.log(response.food_item);
-        getFoodItems(response.food_item[0],response.food_item.length);
-        // console.log(foodItems);
-    }
+    getFoodItems(parseInt(firstKey), countCards);
+    // console.log(foodItems);
+  }
 }
+
 
 var m = 0;
-function getFoodItems(i,length) {
-    // console.log('food url: '+food_url+i);
+function getFoodItems(i, length) {
+  // console.log('food url: '+food_url+i);
 
-    if (m>=length) {
-        console.log('food items array: '+ foodItems);
-        setItems(foodItems);
-        return;
-    }
+  if (m >= length) {
+    console.log("food items array: " + foodItems);
+    setItems(foodItems);
+    return;
+  }
 
-    const foodRequest = new XMLHttpRequest();
-    foodRequest.open('GET',food_url+i,true);
-    foodRequest.onload=()=>{
-        foodItems.push(foodRequest.responseText);
-        // console.log('item: '+foodRequest.responseText)
-        m++;
-        getFoodItems(i+1,length);
+  const foodRequest = new XMLHttpRequest();
+  foodRequest.open("GET", food_url + i, true);
+  foodRequest.onload = () => {
+    foodItems.push(foodRequest.responseText);
+    // console.log('item: '+foodRequest.responseText)
+    m++;
+    getFoodItems(i + 1, length);
+  };
+  foodRequest.setRequestHeader(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaGl2YW1AZ21haWwuY29tIiwiZXhwIjoxNjgxNjMxMDY0LCJpYXQiOjE2ODE2MjkyNjR9.M7iVZ8npycgcnGg0-G7DJKAwxD8A9IC1ThbrdVrOAAoXaBBRPeWuis438e_7HEI-J9gFfL7fHc-LJ77UyRbMvQ"
+  );
+  foodRequest.send();
 
-    }
-    foodRequest.setRequestHeader('Authorization','Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaGl2YW1AZ21haWwuY29tIiwiZXhwIjoxNjgxNDg5MzY3LCJpYXQiOjE2ODE0ODc1Njd9.Cc6L3cUFtVrssO9HxiARdCBkzOuY9yJM3uEX_Y0wtBWoFTSg3UYdbAg9DqOeZGtRvYUEnc_lZa49aggJ19Wm6A');
-    foodRequest.send();
-
-    // if (foodRequest.readyState === XMLHttpRequest.DONE && foodRequest.status === 200) {
-    //     console.log('food items array: '+ foodItems);
-    // }
+  // if (foodRequest.readyState === XMLHttpRequest.DONE && foodRequest.status === 200) {
+  //     console.log('food items array: '+ foodItems);
+  // }
 }
 
-function setItems(foods){
+function setItems(foods) {
 
-    // console.log(container);
+    console.log(Object.values(food_ids));
+    var i = 0;
+  // console.log(container);
+    // console.log('apna cart ' +JSON.stringify(cart))
+  var str = foods
+    .map((item) => {
+      const element = JSON.parse(item);
+        // console.log(JSON.parse(element))
+        // console.log(element)
+        const itemId = element.itemId;
+        // console.log('count : '+ food_ids.itemId);
+      return `
+        <div class="card">
+          <div class="card-container">
+            <img src="${
+              imgURL + itemId
+            }" alt="item image" class="item-image" />
+            <h3 class="item-name">${element.title}</h3>
+            <div class="card-btns">
+              <button class="increase">
+                <i class="fa fa-plus-square"></i>
+              </button><span>${Object.values(food_ids)[i++]} </span
+              ><button class="decrease">
+                <i class="fa fa-minus-square"></i>
+              </button>
+            </div>
+            <span>$${cart.total}</span>
+            <button class="trash-btn">
+              <i class="fa fa-trash"></i>
+            </button>
+          </div>
+        </div>
+`;
+    })
+    .join("");
 
-    var str = foods.map((item)=>{
-        const element = JSON.parse(item);
-
-        return ``;
-
-        
-    }).join('');
-
-    // <!--single item here -->
-    //       <div class="card">
-    //         <div class="card-container">
-    //           <img src="" alt="item image" class="item-image" />
-    //           <h3 class="item-name">item name</h3>
-    //           <div class="card-btns">
-    //             <button class="incease">
-    //               <i class="fa fa-plus-square"></i>
-    //             </button><span>0</span
-    //             ><button class="decrease">
-    //               <i class="fa fa-minus-square"></i>
-    //             </button>
-    //           </div>
-    //           <span>$12</span>
-    //           <button class="trash-btn">
-    //             <i class="fa fa-trash"></i>
-    //           </button>
-    //         </div>
-    //       </div>
-
-    container.innerHTML = str;
-
+  container.innerHTML = str;
 }
