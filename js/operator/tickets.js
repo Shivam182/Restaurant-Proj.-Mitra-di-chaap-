c5e3154bba3dd2835dbe5f86eb3da3297d66fb8b
Shivam Yadav <yadavshivam24012002@gmail.com>
2023-11-06 17:37:46 +0530

    ticketing op

--- js/operator/tickets.js
+++ js/operator/tickets.js
@@ -1,34 +1,29 @@
 import { getElement, getStorage } from "../../utils/util.js";
 
-const searchName = getElement('#find-name');
-const searchNameBtn = getElement('.find-name')
-const searchUserId = getElement('#find-userid')
-const searchUserIdBtn = getElement('.find-userid')
-const searchTime = getElement('#find-time');
-const searchTimeBtn = getElement('.find-time');
+const searchName = getElement("#find-name");
+const searchNameBtn = getElement(".find-name");
+const searchUserId = getElement("#find-userid");
+const searchUserIdBtn = getElement(".find-userid");
+const searchTime = getElement("#find-time");
+const searchTimeBtn = getElement(".find-time");
 
-const searchTable = getElement('#find-table');
-const searchTableBtn = getElement('.find-table');
+const searchTable = getElement("#find-table");
+const searchTableBtn = getElement(".find-table");
 
-const ticketsTable = getElement('.tickets-table');
+const ticketsTable = getElement(".tickets-table");
 
+var usertoken = getStorage("token");
 
-// BIND THE SETITEMS CALLBACK WITH RELATIVE HTTP REQUEST
-
-
-var usertoken = getStorage('token');
-
-if(usertoken.length == 0) {
-    location.href = `login.html`;
+if (usertoken.length == 0) {
+  location.href = `login.html`;
 }
 
 var alltktUrl = `http://localhost:9090/api/ticketing/all`;
 
-var searchIdUrl
-var searchNameUrl 
-var searchTableUrl 
-var searchTimeUrl
-
+var searchIdUrl;
+var searchNameUrl;
+var searchTableUrl;
+var searchTimeUrl;
 
 var httpRequest1;
 var httpRequest2;
@@ -36,119 +31,148 @@ var httpRequest3;
 var httpRequest4;
 var httpRequest5;
 
+getAllTickets(alltktUrl);
 
-getAllTickets(alltktUrl)
+searchNameBtn.addEventListener("click", () => {
+  const text = searchName.value;
 
+  if (text == "") {
+    alert("Please Provide Input");
+  } else {
+    searchNameUrl = `http://localhost:9090/api/ticketing/tickets/name/` + text;
+    searchNameReq(searchNameUrl);
+    ticketsTable.innerHTML = "";
+  }
+});
 
-searchNameBtn.addEventListener('click', ()=>{
+searchTimeBtn.addEventListener("click", () => {
+  const text = searchTime.value;
 
-    const text = searchName.value;
+  if (text == "") {
+    alert("Please Provide Input");
+  } else {
+    searchTimeUrl = `http://localhost:9090/api/ticketing/tickets/time/` + text;
+    searchTimeReq(searchTimeUrl);
+    ticketsTable.innerHTML = "";
+  }
+});
 
-    if(text == "") {
-        alert('Please Provide Input');
-    }else{
-        
-        searchNameUrl = `http://localhost:9090/api/ticketing/tickets/name`;
-        searchNameReq(searchNameUrl);
-        ticketsTable.innerHTML = "";
+searchUserIdBtn.addEventListener("click", () => {
+  const text = searchUserId.value;
 
+  if (text == "") {
+    alert("Please Provide Input");
+  } else {
+    searchIdUrl = `http://localhost:9090/api/ticketing/tickets/user/` + text;
+    searchUserIdReq(searchIdUrl);
+    ticketsTable.innerHTML = "";
+  }
+});
 
-    }
+searchTableBtn.addEventListener("click", () => {
+  const text = searchTable.value;
 
-})
-
-searchTimeBtn.addEventListener('click', ()=>{
-const text = searchTime.value;
-
-    if(text == "") {
-        alert('Please Provide Input');
-    }else{
-        searchTimeUrl  = `http://localhost:9090/api/ticketing/tickets/time`;
-        searchTimeReq();
-        ticketsTable.innerHTML = "";
-
-        
-    }
-})
-
-searchUserIdBtn.addEventListener('click', ()=>{
-    const text = searchUserId.value;
-
-    if(text == "") {
-        alert('Please Provide Input');
-    }else{
-        searchIdUrl  = `http://localhost:9090/api/ticketing/tickets/user`;
-        searchUserIdReq(searchIdUrl);
-        ticketsTable.innerHTML = "";
-
-        
-    }
-})
-
-
-searchTableBtn.addEventListener('click', ()=>{
-    const text = searchTable.value;
-
-    if(text == "") {
-        alert('Please Provide Input');
-    }else{
-
-        searchTableUrl = `http://localhost:9090/api/ticketing/tickets/size`;
-        searchTableReq(searchTableUrl);
-        ticketsTable.innerHTML = "";
-
-        
-    }
-})
+  if (text == "") {
+    alert("Please Provide Input");
+  } else {
+    searchTableUrl = `http://localhost:9090/api/ticketing/tickets/size/` + text;
+    searchTableReq(searchTableUrl);
+    ticketsTable.innerHTML = "";
+  }
+});
 
 function getAllTickets(url) {
+  httpRequest1 = new XMLHttpRequest();
 
-    httpRequest1 = new XMLHttpRequest();
+  httpRequest1.open("GET", url, true);
+  httpRequest1.setRequestHeader("Authorization", usertoken);
 
-    httpRequest1.open('GET', url, true);
-    httpRequest1.setRequestHeader('Authorization', usertoken);
-    httpRequest1.onreadystatechange = setItems;
-    httpRequest1.send();
-
+  var req = httpRequest1;
+  httpRequest1.onreadystatechange = setItems.bind(req);
+  httpRequest1.send();
 }
 
 function searchNameReq(url) {
-    httpRequest2 = new XMLHttpRequest();
+  httpRequest2 = new XMLHttpRequest();
 
-    httpRequest2.open('GET', url, true);
-    httpRequest2.setRequestHeader('Authorization', usertoken);
-    httpRequest2.onreadystatechange = setItems;
-    httpRequest2.send();
+  httpRequest2.open("GET", url, true);
+  httpRequest2.setRequestHeader("Authorization", usertoken);
+  var req = httpRequest2;
+  httpRequest2.onreadystatechange = setItems.bind(req);
+  httpRequest2.send();
 }
 
 function searchTimeReq(url) {
-    httpRequest3 = new XMLHttpRequest();
+  httpRequest3 = new XMLHttpRequest();
 
-    httpRequest3.open('GET', url, true);
-    httpRequest3.setRequestHeader('Authorization', usertoken);
-    httpRequest3.onreadystatechange = setItems;
-    httpRequest3.send();
+  httpRequest3.open("GET", url, true);
+  httpRequest3.setRequestHeader("Authorization", usertoken);
+  var req = httpRequest3;
+  httpRequest3.onreadystatechange = setItems.bind(httpRequest3);
+  httpRequest3.send();
 }
 
 function searchTableReq(url) {
-    httpRequest4 = new XMLHttpRequest();
+  httpRequest4 = new XMLHttpRequest();
 
-    httpRequest4.open('GET', url, true);
-    httpRequest4.setRequestHeader('Authorization', usertoken);
-    httpRequest4.onreadystatechange = setItems;
-    httpRequest4.send();
+  httpRequest4.open("GET", url, true);
+  httpRequest4.setRequestHeader("Authorization", usertoken);
+
+  var req = httpRequest4;
+  httpRequest4.onreadystatechange = setItems.bind(req);
+  httpRequest4.send();
 }
 
 function searchUserIdReq(url) {
-    httpRequest5 = new XMLHttpRequest();
+  httpRequest5 = new XMLHttpRequest();
 
-    httpRequest5.open('GET', url, true);
-    httpRequest5.setRequestHeader('Authorization', usertoken);
-    httpRequest5.onreadystatechange = setItems;
-    httpRequest5.send();
+  httpRequest5.open("GET", url, true);
+  httpRequest5.setRequestHeader("Authorization", usertoken);
+
+  var req = httpRequest5;
+  httpRequest5.onreadystatechange = setItems.bind(req);
+  httpRequest5.send();
 }
 
+function setItems() {
+  // console.log(this)
+  if (this.readyState == XMLHttpRequest.DONE && this.status === 200) {
+    var response = JSON.parse(this.responseText);
 
-function setItems () {
+    // console.log(JSON.stringify(response));
 
+    var str = "";
+
+    // anything else except zero is considered to be as true
+    if (response.length) {
+      str = response
+        .map((item) => {
+          return `
+          <tr>
+          <td>${item.id}</td>
+          <td>NA</td>
+          <td>${item.name}</td>
+          <td>${item.time}</td>
+          <td>${item.tableSize}</td> 
+          </tr> 
+              `;
+        })
+        .join("");
+
+      ticketsTable.innerHTML = `
+    <table>
+    <tr>
+    <th>TID</th>
+    <th>Date</th>
+    <th>Name</th>
+    <th>Time</th>
+    <th>Table Size</th>
+    </tr>
+    ${str}
+    </table>
+    `;
+    } else {
+      alert('No Data !!')
+    }
+  }
 }
